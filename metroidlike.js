8a1a7aed4597803182d8a7df4c79dd5b7fe36cfe
tweeres04 <tweeres04@gmail.com>
2014-02-01 11:13:34 -0800

    Scaffolding

---
+++ metroidlike.js
@@ -0,0 +1,19 @@
+(function(){
+	function startGame(){
+		game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'container', { preload: preload, create: create, update: update });
+	}
+
+	function preload() {
+		
+	}
+
+	function create() {
+		
+	}
+
+	function update() {
+		
+	}
+
+	startGame();
+})();
